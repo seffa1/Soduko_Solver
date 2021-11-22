c02501075b4dcadc17bcf27c58dadc0ed23530b8
seffa_work <sameffa1@gmail.com>
2021-11-22 15:52:11 -0500

    "Added difficulty selection, only easy is available so far"

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+import boards
+import os
+
+
+def clear():
+    os.system('cls')
+
+
+class Board:
+    board = None
+
+    @classmethod
+    def load_board(cls, difficulty):
+        cls.board = boards.boards[difficulty]
+
+
+def select_difficulty():
+    choices = ['1']  # add 2 and 3 here later
+
+    a = ''
+    while a not in choices:
+        clear()
+        print('Select Difficulty:')
+        print('-----------------')
+        print('1 <--- Easy')
+        print('2 <--- Medium')
+        print('3 <--- Hard\n')
+        a = input('--->')
+
+    Board.load_board(a)
+
 
 if __name__ == '__main__':
-    pass
-
-
+    select_difficulty()
